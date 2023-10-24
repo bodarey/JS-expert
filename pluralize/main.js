9c815af63a41e7d037c67b24e61d95d174c23e4b
andy <andy@andy-Lenovo-ideapad-330-15IGM>
2023-10-24 23:00:07 +0100

    add folder with pluralize function

---
+++ pluralize/main.js
@@ -0,0 +1,16 @@
+function pluralize(n){
+	switch (n) {
+		case 1:
+			console.log('vorona');
+			break;
+		case 2,3,4:
+			console.log('voroni');
+			break;
+		default:
+			console.log('voron');
+			break;
+	}
+
+}
+
+pluralize(5);
