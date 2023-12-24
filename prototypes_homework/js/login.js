c9e997bc6f9cd8e129a2946b02dbd5bf5d8458c2
bodarey <69037258+bodarey@users.noreply.github.com>
2023-12-24 18:18:30 +0000

    add library.js

--- prototypes_homework/js/login.js
+++ prototypes_homework/js/login.js
@@ -12,50 +12,24 @@ LoginForm.prototype = {
 	initComponent : function (){
 		//
 		var btn = document.querySelector('.btn-block');
-		var itemMenu = document.querySelector('.bg-white');
-
-		const butonForm = () => {
+		const butonForm = () => { // function to enter the form if is valid or not
 				if (this.validator.isValid()){
-					showHideBlock(form,'hide');
-	  				showHideBlock(dangerBlock,'hide');
-	  				showHideBlock(main2,'show');
-	  				showHideBlock(menu,'show');
-				} else{
-					showHideBlock(form,'show');
-				 	showHideBlock(dangerBlock,'show');
-					showHideBlock(main2,'hide');
-					showHideBlock(menu,'hide');
+					t(false,form,dangerBlock);
+	  				t(true,main2,menu);
+	  				menu.firstElementChild.nextElementSibling.firstElementChild.classList.remove('text-dark');
+					menu.firstElementChild.nextElementSibling.firstElementChild.classList.add('text-danger');
+					createPageUser();
+				} else{					
+					t(false,main2,menu);
+					t(true,dangerBlock,form);
 				}
 			}
 		btn.addEventListener('click',butonForm);
-//////////////////// event listener for menu links to change colors in red and in text-dark
-	    
-		itemMenu.firstElementChild.nextElementSibling.firstElementChild.classList.remove('text-dark');
-		itemMenu.firstElementChild.nextElementSibling.firstElementChild.classList.add('text-danger');
-		var f = (event) => {	
-			var a = [];
-			a.push(itemMenu.firstElementChild.nextElementSibling.firstElementChild);
-			a.push(itemMenu.firstElementChild.nextElementSibling.lastElementChild);
-			a.push(itemMenu.lastElementChild);
-			a.forEach(function(elem){ 
-				if (!elem.classList.contains('text-dark')) {
-					elem.classList.add('text-dark');
-				}
-			 });
-			a.forEach(function(elem){ 
-				if (!elem.classList.contains('text-danger')) {
-					elem.classList.add('text-danger');
-				}
-			 });
-			a.forEach(function(elem){
-				if (elem == event.target) {
-					elem.classList.remove('text-dark');
-				}
-			});
-		};
-
-		itemMenu.addEventListener('click',f);
-//////////////////
+		
+	
+		
+	
+		//////////////////
 		
 	},
 	validateUserData : function (){
--- prototypes_homework/js/validator.js
+++ prototypes_homework/js/validator.js
@@ -1,33 +1,4 @@
 
-var dangerBlock = document.querySelector('.alert-danger');
-var inputEmail = document.getElementById('inputEmail');
-var inputPassword = document.getElementById('inputPassword');	
-var menu = document.querySelector('.d-flex');
-var form = document.querySelector('.form-signin');
-menu.classList.add('d-none');
-dangerBlock.classList.add('hide');
-var main1 = document.querySelectorAll('main')['0'];
-var main2 = document.querySelectorAll('main')['1'];
-main2.classList.add('d-none');
-
-inputEmail.addEventListener('keypress',function(){showHideBlock(dangerBlock, 'hide') });
-inputPassword.addEventListener('keypress',function(){showHideBlock(dangerBlock, 'hide') });
-
-////////
-function showHideBlock(block,action){
-        block.classList.remove('show');
-        block.classList.remove('hide');
-        block.classList.remove('d-none');
-        if (action == 'hide') {
-            block.classList.add('hide');
-            block.classList.add('d-none');
-        } else if (action == 'show'){
-            block.classList.add('show');
-        }
-}
-////////
-
-
 
 function Validator(){
 	
@@ -35,7 +6,7 @@ function Validator(){
 //////////
 Validator.prototype = {
 	isValid: function(){
-		/////////
+
 		
     //////////////////check if data from input form is equal with data from localstorage
        	if (checkValidEmail()) {
@@ -61,13 +32,11 @@ Validator.prototype = {
 }
 
 //////////
-//var v = new Validator()
-//var btn = document.querySelector('.btn-block');
-//btn.addEventListener('click',v.isValid);
 
-//var itemMenu = document.querySelector('nav');
-//var b = itemMenu.firstElementChild;
-//console.log(itemMenu.lastElementChild);
-//console.log(itemMenu.nextElementSibling);
-//b.classList.add('opacity-100');
-//b.classList.add('text-danger');
+
+
+
+
+ 
+		
+
