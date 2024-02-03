47577196b3e345469d3bde41aabe7819a0fd6329
bodarey <69037258+bodarey@users.noreply.github.com>
2024-02-03 20:02:13 +0000

    ES6

---
+++ ES6/js/library.js
@@ -0,0 +1,14 @@
+
+var dangerBlock = document.querySelector('.alert-danger');
+var inputEmail = document.getElementById('inputEmail');
+var inputPassword = document.getElementById('inputPassword');	
+var menu = document.querySelector('.d-flex');
+var form = document.querySelector('.form-signin');
+var main1 = document.querySelectorAll('main')['0'];
+var main2 = document.querySelectorAll('main')['1'];
+var userPage = document.querySelectorAll('main')['2'];
+
+/////////////////////////////////////////////////////////////////
+
+//////////////////////
+
